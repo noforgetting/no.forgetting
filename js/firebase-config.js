70a194f7509c16f51cf2dd06da1baaf12e6d7cb8
NoForgetting <tepyxlplayz@gmail.com>
2026-08-26 19:55:09 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,12 @@
 // contains no project values. Copy the values from Firebase Console > Project
 // settings > Your apps. Do not put a service-account key in this project.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDd7iuAdHxOS_4ugP7dT86s44_CRWiOTPc",
+  authDomain: "noforgetting-database.firebaseapp.com",
+  projectId: "noforgetting-database",
+  storageBucket: "noforgetting-database.firebasestorage.app",
+  messagingSenderId: "578801937014",
+  appId: "1:578801937014:web:e1b1c04d04a6f85fe84d57",
 };
 
 // Optional: create a reCAPTCHA v3 key in Firebase App Check and place its
